252e0d64930940edd5de5b9ea8541afc86fc3dff
karlesleith <karlesleith@gmail.com>
2017-09-20 16:46:15 +0100

    FizzBuzz.py added

---
+++ FizzBuzz.py
@@ -0,0 +1,14 @@
+import math
+
+for n in list(range(0,101)):
+	word = ""
+	if(n % 3 == 0): # if i is divisable by 3
+		word += 'Fizz'
+	if(n % 5 == 0):# if i is divisable by 5
+		word += 'Buzz'
+	if(word == ""):
+		word += str(i)
+
+	print(word)
+		
+print('\n Do Do Doooo, GoodBye!')
